cb9911e1000047c9f1c3df9c474cbae9a807c3aa
prathyand <prathyand@gmail.com>
2021-11-09 03:53:47 -0500

    Updated code comments

--- AnimatedQuintris.py
+++ AnimatedQuintris.py
@@ -1,8 +1,3 @@
-# "Fancier" animated interactive version of quintris. v0.2
-# D. Crandall, Sept 2021
-#
-# DON'T MODIFY THIS FILE! Or else we might not be able to grade your submission properly.
-#
 
 from QuintrisGame import *
 
